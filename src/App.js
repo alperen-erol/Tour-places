2643a8b1153e8e2db19a333155708a0552c037df
alperen-erol <alpereneroltr@gmail.com>
2023-06-19 06:23:29 +0300

    footer & further improvements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import "./app.scss"
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
    <>
    <Header/>
    <Main/>
+   <Footer/>
    </>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React from "react";
+import "./footer.scss";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="footercontainer">
+        <div className="text">
+          <span className="foottext">Reach Me:</span>
+          <a
+            className="link"
+            href="https://github.com/alperen-erol"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          <a
+            className="link"
+            href="<span>https://www.linkedin.com/in/alperen-erol-75a17a257/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
